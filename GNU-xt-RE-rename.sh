d3da514247574c6b6034f9807313b13ca671444e
abdulbadii <abdulbadii@yandex.com>
2019-02-08 19:56:49 +0700

    Update GNU-xt-RE-rename.sh

--- GNU-xt-RE-rename.sh
+++ GNU-xt-RE-rename.sh
@@ -7,18 +7,18 @@ let i++
 case ${a:0:2} in
 -t) T=1;;
 -c) c=-regex;I=;;
--A-Sa-suvz) o="$o $a";;
+-[A-Sa-suvz]) o="$o $a";;
 *)
 if [[ "$@" =~ ' ;;' ]]
 then
 x=${a%[^ ]*;;*};x=$x${a:${#x}:1}
-y=${s#* ;;}
+y=${a#* ;;}
 # PCRE --> GNU-ext regex
 x=`echo $x |sed -E 's/(\[.*?)\\\w([^]]*\])/\1a-z0-9\2/g; s/(\[.*?)\\\d([^]]*\])/\10-9\2/g ;s/\\\d/[0-9]/g; s/([^\])\.\*/\1[^\/]*/g; s/\*\*/.*/g'`
 r=${x%%[!.*]*}
 [[ "$r" =~ \.\* ]]&&{ x=${x:${#r}};r='.*'; }
 # below RHS after $ '\n' for Linux, Windows port (Msys/mingw): '\r\n', Mac port: '\r'
-IFS=$'\n'
+IFS=$'\r\n'
 l==;while([ "$l" ])
 do l=
 	if [[ "$x" =~ ^\(*/ ]] ;then
@@ -26,7 +26,7 @@ do l=
 		for F in `find ${s%/*} -regextype posix-extended $c "$x" |head -n99`
 		{
 			t=`echo $F | sed -E "s|$x|$y|$I"`
-			if [ $T ] ;then echo "Would rename $F -> $t"
+			if [ $T ] ;then echo -e Would rename '\033[1;36m' "$F -> $t" '\033[0m' 
 			else 
 				mkdir -p "${t%/*}"
 				mv -bvS .old $o "$F" "$t"
@@ -36,7 +36,7 @@ do l=
 		for F in `find ~+ -type f -regextype posix-extended $c "$PWD/$r$x" |head -n99`
 		{
 		t=`echo $F | sed -E "s|$x|$y|$I"`
-		if [ $T ] ;then echo "Would rename $F -> $t"
+		if [ $T ] ;then echo -e Would rename '\033[1;36m' "$F -> $t" '\033[0m' 
 		else
 			mkdir -p "${t%/*}"
 			mv -bvS .old $o "$F" "$t"
