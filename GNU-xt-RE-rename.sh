5ad7386e4a9d8b9c0564a43d73f49d556f83f83f
abdulbadii <abdulbadii@yandex.com>
2019-12-30 12:48:58 +0700

    Update GNU-xt-RE-rename.sh

--- GNU-xt-RE-rename.sh
+++ GNU-xt-RE-rename.sh
@@ -1,8 +1,9 @@
 #! /usr/bin/bash
 ren(){
-[[ "$1" =~ -h|--help ]]&&{ echo -e For more help go to https://github.com/abdulbadii/GNU-ext-regex-rename/blob/master/README.md
+[[ "$1" =~ -h|--help ]]&&{ echo -e "\nFor more help go to https://github.com/abdulbadii/GNU-ext-regex-rename/blob/master/README.md"
 	mv --help|sed -Ee 's/\bmv\b/ren/;8a\ \ -c\t\t\t\tCase sensitive search' -e '14a\ \ -N\t\t\t\tNot to really execute only tell what it will do. It is useful as a test' ;}
-N=;i=;o=;c=-iregex;I=i;
+unset f N i o
+c=-iregex;I=i;
 if [[ "${@: -1}" =~ ' ;;' ]];then
 for a
 {
@@ -13,21 +14,22 @@ case ${a:0:6} in
 -[HLPRSTabdfilnpstuvxz]) o=$o$a\ ;;
 -*) echo Unrecognized option \'$a\';return;;
 *)
-if [ ! -f $f ];then
+if [ -n "$f" ] && [ ! -f "$f" ];then
 	f=$(echo $f| sed -E 's~\\\\~\\~g ;s~\\~/~g ;s~\b([a-z]):(/|\W)~/\1/~i')
 	[ -f $f ]||{ echo file does not exist;return;}
 fi
-y=${a#* ;;}
-x=${a%[^ ]*;;*};x=$x${a:${#x}:1}
+[[ $a =~ ^(.+)\;\;[\ ]*(.+)$ ]]
+x=${BASH_REMATCH[1]};y=${BASH_REMATCH[2]}
+
 # PCRE --> GNU-ext regex
-x=$(echo $x |sed -E 's/(\[.*?)\\\w([^]]*\])/\1a-z0-9\2/g; s/(\[.*?)\\\d([^]]*\])/\10-9\2/g ;s/\\\d/[0-9]/g; s/([^\])\.\*/\1[^\/]*/g; s/\.?\*\*/.*/g')
+x=$(echo $x |sed -E 's/(\[.*?)\\\w([^]]*\])/\1a-z0-9\2/g; s/(\[.*?)\\\d([^]]*\])/\10-9\2/g ;s/\\\d/[0-9]/g; s/([^\])\.\*/\1[^\/]*/g; s/\.?\*\*/.*/g ;s/\s+$//')
 v=${x#.\*}
 if [[ "$x" =~ ^\(*(/|[a-z]:) ]] ;then
 	s=$(echo $x |sed -E 's/([^[|*+\\{.]+).*/\1/;s~(.+)/.*~\1~;s/[()]//g')	#the first longest literal
 else s=~+;x=$PWD/$x
 fi
 IFS=$'\n';LC_ALL=C
-if((N==1));then
+if((N));then
 	if [ "$f" ];then	while read -r F
 	do	[ -e $F ] ||{ F=$(echo $F|sed -E 's/[^!#-~]*([!#-~]+)[^!#-~]*/\1/;s~(\\+|//+)~/~g;s~\b([a-z]):(/|\W)~/\1/~i')
 			[ -e $F ] ||continue;}
@@ -54,13 +56,14 @@ if((N==1));then
 	}
 	fi
 else
+	B='-bS .old'
 	if [ "$f" ];then	while read -r F
 	do	[ -e $F ] ||{ F=$(echo $F|sed -E 's/[^!#-~]*([!#-~]+)[^!#-~]*/\1/;s~(\\+|//+)~/~g;s~\b([a-z]):(/|\W)~/\1/~i')
 			[ -e $F ] ||continue;}
 		t=`echo $F | sed -E "s!$v!$y!$I"`
 		[ $F = $t ]||{
 		mkdir -p "${t%/*}"
-		command mv -bS .old $o "$F" "$t" &&{
+		command mv  $o "$F" "$t" &&{
 		echo -ne '\033[0;36m'
 		if [ ${F%/*} = ${t%/*} ];then	echo -e Renaming'\033[0m' $F -\> $t
 		elif [ ${F##*/} = ${t##*/} ];then	echo -e Moving'\033[0m' $F -\> $t
@@ -76,7 +79,7 @@ else
 		t=`echo $F | sed -E "s|$v|$y|$I"`
 		[ $F = $t ]||{
 		mkdir -p "${t%/*}"
-		command mv -bS .old $o "$F" "$t" &&{
+		command mv $o "$F" "$t" &&{
 		echo -ne '\033[0;36m'
 		if [ ${F%/*} = ${t%/*} ];then	echo -e Renaming'\033[0m' $F -\> $t
 		elif [ ${F##*/} = ${t##*/} ];then	echo -e Moving'\033[0m' $F -\> $t
@@ -88,6 +91,6 @@ else
 fi
 unset IFS;;esac
 }
-else	t=xv${@: -1};mkdir -p "${t%/*}";mv -bvS .old $o ${@: -2} $t
+else	t=${@: -1};mkdir -p "${t%/*}";mv -v $o ${@: -2} $t
 fi
 }
